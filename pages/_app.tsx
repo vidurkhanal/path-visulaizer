8ac188d8675088b531bb1e885a70d5907e9327ce
vidurkhanal <vkhanal@caldwell.edu>
2022-11-19 13:46:31 -0500

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ReactElement } from "react";
+
+export default function App({
+    Component,
+    pageProps,
+}: AppProps): ReactElement<any, any> {
+    return <Component {...pageProps} />;
+}
